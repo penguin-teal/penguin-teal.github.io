fdc8b5216ba5300c207323d2e1676358c0a81500
penguin-teal <_penguin@tuta.io>
2023-11-15 20:49:19 -0600

    Syntax highlighter, header IDs and permalinking

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,44 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
+import remarkToc from 'remark-toc';
+import rehypeSlug from 'rehype-slug';
+import rehypeAutolinkHeadings from 'rehype-autolink-headings';
+
 // https://astro.build/config
 export default defineConfig({
+    markdown: {
+        remarkPlugins: [ remarkToc ],
+        rehypePlugins: [ rehypeSlug, [rehypeAutolinkHeadings, { behavior: 'wrap' }] ],
+        shikiConfig: {
+          // Choose from Shiki's built-in themes (or add your own)
+          // https://github.com/shikijs/shiki/blob/main/docs/themes.md
+
+          // Alternatively, provide multiple themes
+          // https://github.com/antfu/shikiji#lightdark-dual-themes
+          themes: {
+            light: 'github-light',
+            dark: 'github-dark',
+          },
+          // Add custom languages
+          // Note: Shiki has countless langs built-in, including .astro!
+          // https://github.com/shikijs/shiki/blob/main/docs/languages.md
+          langs: [ "c" ],
+          // Enable word wrap to prevent horizontal scrolling
+          wrap: true,
+        }
+    },
 	integrations: [
 		starlight({
-			title: 'My Docs',
+			title: 'penguin-teal',
 			social: {
-				github: 'https://github.com/withastro/starlight',
+				github: 'https://github.com/penguin-teal/penguin-teal.github.io',
 			},
 			sidebar: [
 				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', link: '/guides/example/' },
-					],
-				},
-				{
 					label: 'Reference',
 					autogenerate: { directory: 'reference' },
-				},
+				}
 			],
 		}),
 	],
